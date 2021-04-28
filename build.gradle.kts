7ec4cda3749871f0caea25bbfb60bc4ce3def034
jamesnyakush <emvesttech@gmail.com>
2021-04-28 19:52:56 +0300

    Finished gradle migration :rocket: :relaxed:

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean").configure {
+    delete("build")
+}
